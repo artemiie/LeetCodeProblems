2e5850f46e2e833d6440f41fbfa0b85e5568b863
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-01-08 18:58:44 +0200

    Add solution 2 for "Two sum" problem

---
+++ src/main/java/leetcode/problems/two_sum/Solution2.java
@@ -0,0 +1,14 @@
+package leetcode.problems.two_sum;
+
+public class Solution2 {
+  public int[] twoSum(int[] nums, int target) {
+    for (int i = 1; i < nums.length; i++) {
+      for (int j = i; j < nums.length; j++) {
+        if (nums[j] + nums[j - i] == target) {
+          return new int[] {j, j - i};
+        }
+      }
+    }
+    return null;
+  }
+}
---
+++ src/test/java/leetcode/problems/two_sum/Solution2Test.java
@@ -0,0 +1,17 @@
+package leetcode.problems.two_sum;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Solution2Test {
+
+  @Test
+  public void test() {
+    Assertions.assertAll(
+        () -> Assertions.assertArrayEquals(new int[] {0, 1}, Solution1.twoSum(new int[] {2, 7, 11, 15}, 9)),
+        () -> Assertions.assertArrayEquals(new int[] {1,2}, Solution1.twoSum(new int[] {3, 2, 4}, 6)),
+        () -> Assertions.assertArrayEquals(new int[] {0, 1}, Solution1.twoSum(new int[] {3, 3}, 6)),
+        () -> Assertions.assertArrayEquals(new int[] {1,3}, Solution1.twoSum(new int[] {3, 5, 6, 2}, 7)),
+        () -> Assertions.assertArrayEquals(new int[] {3, 4}, Solution1.twoSum(new int[] {0, -1, 2, -3, 1}, -2)));
+  }
+}
