a4b314ca5f4def088a2dcc2c3033e281b4054a47
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-01-09 20:05:12 +0200

    Add solution 1 for "Palindrome number" problem

---
+++ src/test/java/leetcode/problems/palindrome_number/Solution1Test.java
@@ -0,0 +1,17 @@
+package leetcode.problems.palindrome_number;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Solution1Test {
+
+  @Test
+  public void test() {
+    Assertions.assertAll(
+        () -> Assertions.assertTrue(Solution1.isPalindrome(121)),
+        () -> Assertions.assertFalse(Solution1.isPalindrome(-121)),
+        () -> Assertions.assertFalse(Solution1.isPalindrome(10)),
+        () -> Assertions.assertTrue(Solution1.isPalindrome(12321)),
+        () -> Assertions.assertFalse(Solution1.isPalindrome(8999)));
+  }
+}
