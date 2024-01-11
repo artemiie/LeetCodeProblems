bfb90cf43951c847e876a680408df36ecdf81216
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-01-12 00:18:55 +0200

    Add solution 1 for "Merge two sorted list" problem

---
+++ src/main/java/leetcode/problems/merge_two_sorted_list/ListNode.java
@@ -0,0 +1,13 @@
+package leetcode.problems.merge_two_sorted_list;
+
+public class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
