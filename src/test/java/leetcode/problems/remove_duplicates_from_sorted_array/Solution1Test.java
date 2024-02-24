86a8df7242622f98da94e61e4109bfd8ee188d65
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-24 13:00:27 +0200

    Fix test

--- src/test/java/leetcode/problems/remove_duplicates_from_sorted_array/Solution1Test.java
+++ src/test/java/leetcode/problems/remove_duplicates_from_sorted_array/Solution1Test.java
@@ -6,7 +6,6 @@ public class Solution1Test {
 
   @Test
   public void test1() {
-
     int[] nums = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
     int[] expectedNums = new int[] {0, 1, 2, 3, 4};
 
@@ -20,7 +19,6 @@ public class Solution1Test {
 
   @Test
   public void test2() {
-
     int[] nums = new int[] {1, 1, 2};
     int[] expectedNums = new int[] {1, 2};
 
@@ -34,7 +32,6 @@ public class Solution1Test {
 
   @Test
   public void test3() {
-
     int[] nums = new int[] {2, 2, 2, 2, 2};
     int[] expectedNums = new int[] {2};
 
@@ -48,7 +45,6 @@ public class Solution1Test {
 
   @Test
   public void test4() {
-
     int[] nums = new int[] {1, 2, 2, 3, 4, 4, 4, 5, 5};
     int[] expectedNums = new int[] {1, 2, 3, 4, 5};
 
@@ -62,7 +58,6 @@ public class Solution1Test {
 
   @Test
   public void test5() {
-
     int[] nums = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
     int[] expectedNums = new int[] {0, 1, 2, 3, 4};
 
