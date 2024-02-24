7c1dd107ef18149b3ff380fb5c5261563867c8e6
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-24 13:03:38 +0200

    Add solution

---
+++ src/main/java/leetcode/problems/remove_duplicates_from_sorted_array/Solution3.java
@@ -0,0 +1,14 @@
+package leetcode.problems.remove_duplicates_from_sorted_array;
+
+public class Solution3 {
+  public static int removeDuplicates(int[] nums) {
+    int i=0;
+    for(int j=1;j<nums.length;j++){
+      if(nums[i]!=nums[j]){
+        i++;
+        nums[i]=nums[j];
+      }
+    }
+    return i+1;
+  }
+}
