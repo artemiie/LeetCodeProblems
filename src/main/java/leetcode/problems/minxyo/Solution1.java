cd069f81feb002a345ae68f8dfd7248d0b4780c4
Artemie <artemie.fliostor@gmail.com>
2024-04-20 19:41:31 +0300

    Add solution

---
+++ src/main/java/leetcode/problems/minxyo/Solution1.java
@@ -0,0 +1,17 @@
+package leetcode.problems.minxyo;
+
+public class Solution1 {
+  public static void main(String[] args) {
+    System.out.println(getLength("YY"));
+    System.out.println(getLength("XX"));
+    System.out.println(getLength("XY"));
+    System.out.println(getLength("YOX"));
+    System.out.println(getLength("OOOXOOYOXO"));
+    System.out.println(getLength("OOOXXOY"));
+    System.out.println(getLength("OXOYYX"));
+  }
+
+  public static int getLength(String text) {
+    return 0;
+  }
+}
